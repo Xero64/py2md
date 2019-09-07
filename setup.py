4b501c4f8f359a5e603c094b2d4340afa5e740fc
Xero64 <xero64@gmail.com>
2019-09-07 15:07:21 +0200

    Increased version and added an entry point.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py2md",
-    version="0.0.3",
+    version="0.0.4",
     author="Xero64",
     author_email="xero64@gmail.com",
     description="Run python code in jupyter to generate markdown reports.",
@@ -13,9 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Xero64/py2md",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
+    entry_points={"console_scripts": ["py2md=py2md.__main__:main"]},
 )
