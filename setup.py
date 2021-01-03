1168166c0acef7dfd33a8716de9c1ec9805f47cf
Xero64 <xero64@gmail.com>
2021-01-03 12:57:07 +0200

    Added GitHub action.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="py2md",
-    version="0.0.10",
+    version="0.0.11",
     author="Xero64",
     author_email="xero64@gmail.com",
     description="Run python code in jupyter to generate markdown reports.",
